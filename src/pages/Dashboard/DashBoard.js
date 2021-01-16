aab4312411f99d1e9005da267abcb219ce7ada86
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-01-16 12:28:32 +0530

    updated

--- src/pages/Dashboard/DashBoard.js
+++ src/pages/Dashboard/DashBoard.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react"
-import { Link, useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import { auth } from "../../firebase/config"
+import UploadForm from '../../components/UploadForm';
 
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -13,6 +14,8 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+
+
 import {
   EventSeat,
   Home,
@@ -22,21 +25,42 @@ import {
   PowerSettingsNew,
   LabelImportant
 } from "@material-ui/icons";
+import useFirestore from "../../hooks/useFirestore";
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 
 const drawerWidth = 240;
-const appBarHeight = 70;
 const themeColor = "rgb(252, 52, 52)";
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
   },
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    height: appBarHeight,
     backgroundColor: themeColor,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
   },
   drawer: {
     width: drawerWidth,
@@ -45,24 +69,42 @@ const useStyles = makeStyles((theme) => ({
   drawerPaper: {
     width: drawerWidth,
   },
-  drawerContainer: {
-    overflow: 'auto',
-  },
-  logoutBtn: {
-    backgroundColor: themeColor,
-    color: "#fff",
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
-
-export default function Dashboard() {
+export default function DashBoard() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
   const [error, setError] = useState("");
   const history = useHistory();
-  const classes = useStyles();
+
+
+  const _collection = "beds";
+  const { docs } = useFirestore(_collection);
 
   async function handleLogout() {
     setError("")
@@ -76,83 +118,119 @@ export default function Dashboard() {
     }
   }
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar display="flex">
-          <Typography variant="h5" Wrap>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
             Admin Dashboard
           </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
         className={classes.drawer}
-        variant="permanent"
+        variant="persistent"
+        anchor="left"
+        open={open}
         classes={{
           paper: classes.drawerPaper,
         }}
       >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-            <ListItem button >
-              <ListItemIcon>
-                <Home />
-              </ListItemIcon>
-              <ListItemText primary="Home" />
-            </ListItem>
-            <ListItem button >
-              <ListItemIcon>
-                <KingBed />
-              </ListItemIcon>
-              <ListItemText primary="Beds" />
-            </ListItem>
-            <ListItem button >
-              <ListItemIcon>
-                <Weekend />
-              </ListItemIcon>
-              <ListItemText primary="Sofas" />
-            </ListItem>
-            <ListItem button >
-              <ListItemIcon>
-                <LabelImportant />
-              </ListItemIcon>
-              <ListItemText primary="Dinning Tables" />
-            </ListItem>
-            <ListItem button >
-              <ListItemIcon>
-                <EventSeat />
-              </ListItemIcon>
-              <ListItemText primary="Charis" />
-            </ListItem>
-            <ListItem button >
-              <ListItemIcon>
-                <Tv />
-              </ListItemIcon>
-              <ListItemText primary="Tv Stands" />
-            </ListItem>
-            <ListItem button >
-              <ListItemIcon>
-                <LabelImportant />
-              </ListItemIcon>
-              <ListItemText primary="Dressing Tables" />
-            </ListItem>
-
-            <Divider />
-            <ListItem button onClick={handleLogout} >
-              <ListItemIcon>
-                <PowerSettingsNew />
-              </ListItemIcon>
-              <ListItemText primary="Log Out" />
-            </ListItem>
-          </List>
-          <Divider />
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
         </div>
-      </Drawer>
-      <main className={classes.content}>
-        <Toolbar />
+        <Divider />
+        <List>
+          <ListItem button >
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon>
+              <KingBed />
+            </ListItemIcon>
+            <ListItemText primary="Beds" />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon>
+              <Weekend />
+            </ListItemIcon>
+            <ListItemText primary="Sofas" />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon>
+              <LabelImportant />
+            </ListItemIcon>
+            <ListItemText primary="Dinning Tables" />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon>
+              <EventSeat />
+            </ListItemIcon>
+            <ListItemText primary="Charis" />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon>
+              <Tv />
+            </ListItemIcon>
+            <ListItemText primary="Tv Stands" />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon>
+              <LabelImportant />
+            </ListItemIcon>
+            <ListItemText primary="Dressing Tables" />
+          </ListItem>
 
+          <Divider />
+          <ListItem button onClick={handleLogout} >
+            <ListItemIcon>
+              <PowerSettingsNew />
+            </ListItemIcon>
+            <ListItemText primary="Log Out" />
+          </ListItem>
+        </List>
+        <Divider />
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <Typography paragraph>
+
+        </Typography>
+        <Typography paragraph>
+
+        </Typography>
       </main>
     </div>
   );
